773ca0b469c7d511b1694edce5ca98474426112a
marina <starostinamb@gmail.com>
2024-03-22 09:17:43 +0300

    update info format

--- src/AppContainer.tsx
+++ src/AppContainer.tsx
@@ -10,7 +10,7 @@ import {
 const App: FC = () => {
   const [ sdk, setSdk ] = useState<JazzSdk | undefined>(undefined);
   const [status, setStatus] = useState('process');
-  const [ info, setInfo ] = useState('');
+  const [ info, setInfo ] = useState({ });
 
   useEffect(() => {
     console.log('Start creating sdk...');
@@ -58,40 +58,51 @@ const App: FC = () => {
         'audio',
         'video',
       ).then(({audio, video}) => {
-        setInfo({
-          promise: 'requestUserMediaPermissions is resolved',
-          audio: JSON.stringify(audio),
-          video: JSON.stringify(video)
-        });
+        setInfo((oldInfo) => ({
+          ...oldInfo,
+          ...{
+            audio: JSON.stringify(audio),
+            video: JSON.stringify(video)
+          }
+        }));
         console.log('!!! never got here !!!');
       });
 
       const { audio, video } = localDevices.userMediaPermissions.get();
-      setInfo({
-        promise: 'requestUserMediaPermissions promise is not resolved',
-        audio: JSON.stringify(audio),
-        video: JSON.stringify(video)
-      });
+      setInfo((oldInfo) => ({
+        ...oldInfo,
+        ...{
+          audio: JSON.stringify(audio),
+          video: JSON.stringify(video)
+        }
+      }));
 
       const audioInputDevices = localDevices.audioInputDevices.get();
       const audioOutputDevices = localDevices.audioOutputDevices.get();
       const videoInputDevices = localDevices.audioOutputDevices.get();
 
-      setInfo({
-        audioInputDevices: JSON.stringify(audioInputDevices),
-        audioOutputDevices: JSON.stringify(audioOutputDevices),
-        videoInputDevices: JSON.stringify(videoInputDevices)
-      });
+      setInfo((oldInfo) => ({
+        ...oldInfo,
+        ...{
+          audioInputDevices: JSON.stringify(audioInputDevices),
+          audioOutputDevices: JSON.stringify(audioOutputDevices),
+          videoInputDevices: JSON.stringify(videoInputDevices)
+        }
+      }));
 
       const audioInput = localDevices.audioInput.get();
       const audioOutput = localDevices.audioOutput.get();
       const videoInput = localDevices.videoInput.get();
 
-      setInfo({
-        audioInput: JSON.stringify(audioInput),
-        audioOutput: JSON.stringify(audioOutput),
-        videoInput: JSON.stringify(videoInput)
-      });
+      setInfo((oldInfo) => ({
+        ...oldInfo,
+        ...{
+          audioInput: JSON.stringify(audioInput),
+          audioOutput: JSON.stringify(audioOutput),
+          videoInput: JSON.stringify(videoInput)
+        }
+      }));
+
     }
   }, [localDevices])
 
@@ -100,11 +111,15 @@ const App: FC = () => {
       <div>
         <div>SDK version: {SDK_VERSION}</div>
         <div>{Object.keys(info).map(key => (
-          <div key={key}>
-            <span>{key}:</span>
-            <span>{info[key]}</span>
+          <div key={ key }>
+            <div>
+              <span>{ key }:</span>
+              <span>{ info[key] }</span>
+            </div>
+            <br />
           </div>
-        ))}</div>
+        )) }
+        </div>
       </div>
     </div>
   );
